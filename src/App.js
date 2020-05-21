a7202fdcd417d78b3dc87c06827e5e0b232e77a1
amine371 <63712749+amine371@users.noreply.github.com>
2020-05-21 12:41:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import './App.css';
-import './style.css';
-import imageInSrc from  '...../orange.jpg';
-import './la  vidéo  from  '../react-player';
-function App() {
-  return (
-    < div  style = { { border : "solid 1px black" , maxWidth : "100vw" } }>  
- 
-        < h1  className = { "title red" } > orange </ h1 >  
+importer  React à  partir de  'react' ;
+importer  imageInSrc  depuis  './assets/imageInSrc.png' ;
+import  './assets/style.css' ;
+import  './App.css' ;
 
-        < Br / >  
-
-        <img  src = {imageInSrc}  alt = "orange" />
-       
-        < Br / > 
-
-        < img  src = { "imageInPublic.jpg" }  alt = "orange" />  
-
-      < / div >  
-
-      < Video   url = { "../myVideo.mp4" }  width = { 320 }  height = { 240 }  contrôles />  
-    < / div >
+fonction  App ( )  {
+  retour  (
+    
+    < div >
+    < div  style = { { border : 'solid 1px black' ,  maxWidth : '100vw' } } >  
+      < h1  className = "title red"  style = { { whiteSpace : 'pre-line' } } > orange < / h1 >  
+      
+      < img  src = { imageInSrc }   width = { 320 }  style = { { display : 'orange' } } / > 
+      < img  src = "/imageInPublic.png"   width = { 320 } / >  
+    < / div >  
+    < video  width = { 320 }  height = { 240 }  contrôles >  
+      < source  src = "/myVideo.mp4"  type = "video / mp4" / > 
+    < / vidéo >
+  < / div >
   ) ;
 }
 
-exporter l' application par défaut;
+exporter l'  application par défaut  ;
